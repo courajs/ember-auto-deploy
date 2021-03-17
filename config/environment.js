621b21560e887fea8458c7c91fd19736fe1ee9f5
Aaron Sikes <aaron@sikes.io>
2021-03-17 19:37:38 -0400

    Set up auto-deploy to GitHub Pages via GitHub Actions

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ember-auto-deploy',
     environment,
-    rootURL: '/',
+    rootURL: process.env['EMBER_ROOT_URL'] || '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
